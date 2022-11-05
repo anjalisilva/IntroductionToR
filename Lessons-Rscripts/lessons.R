75c1f8aaa7be7e11dffae4138a3411e26c80e55c
Anjali Silva <a.silva@utoronto.ca>
2022-11-05 10:26:18 -0400

    Create lessons.R

---
+++ Lessons-Rscripts/lessons.R
@@ -0,0 +1,9 @@
+#### Class 01 ####
+# Author: Anjali Silva
+# Date: 7 Nov 2022
+# Lessons: 00-Intro; 01-Hello world; 02-Work Practices
+
+# location
+getwd() # get working directory
+
+setwd("/Users/as/Desktop/DSI-IntroR") # don't do manually
