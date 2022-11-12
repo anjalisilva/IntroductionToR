90ffbca0748c8cd4ea900dc9a3313c3e778241be
Anjali Silva <a.silva@utoronto.ca>
2022-11-12 12:17:50 -0500

    Nov 12 instructor script submitted

--- Lessons-Rscripts/lessons.R
+++ Lessons-Rscripts/lessons.R
@@ -86,7 +86,7 @@ c(xm, mean(x, trim = 0.10))
 # End of class 7 Nov 
 
 
-# Class 2 ####
+# Class 02 ####
 # Author: Anjali Silva
 # Date: 10 Nov 2022
 # Lessons: 01, 02, 03
@@ -300,5 +300,322 @@ namedVector
 
 # functionName() # round brackets for function names
 # End of class 10 Nov 
+
+# Class 03 ####
+# Author: Anjali Silva
+# Date: 12 Nov 2022
+# Lessons: 03, 04
+
+getwd() # check working directory
+
+# Subsetting
+# Accessing elements
+
+namedVector <- c(a = 100, b = 200, c = 80)
+namedVector
+names(namedVector) # "a" "b" "c"
+namedVector[1]
+namedVector["a"]
+namedVector[1:2]
+namedVector[1:3]
+namedVector[1:4]
+namedVector[-1] # removed first position
+namedVector[-c(1:2)]
+namedVector[c("a", "b")] # accessing elements a and b
+
+namedVector[c(1, -3)] # cannot mix positive and
+# negative integers when accessing vectors
+# only 0's may be mixed with negative subscripts
+
+namedVector[c(0, -3)] 
+namedVector[-3]
+namedVector[1]
+
+
+# Lists - heterogeneous
+myList <- list(7.5, "list", TRUE, 4+1i, 4L)
+myList
+typeof(myList) # list
+myList[1]
+myList[[1]]
+typeof(myList[[1]]) # double
+typeof(myList[[2]]) # "character"
+length(myList) # 5
+str(myList)
+?str
+# indexing in R: section 3.4 Indexing
+# URL: https://cran.r-project.org/doc/manuals/R-lang.html#Indexing
+
+# vectors 1D - take length
+# structures 2D - dimension
+
+# Tibbles are 2D structures from tidyverse package
+# Tibble columns have to be the same length
+# Each column has to have the same data type
+
+# install.packages("tidyverse")
+library("tidyverse")
+myTibble <- tibble::tibble(x = c(1:5),
+                           y = letters[1:5],
+                           z = x + 2) # x is a variable
+myTibble # 5 rows x 3 columns
+dim(myTibble) # 5 3
+class(myTibble)
+
+data() # access built-in datasets in R
+iris
+head(iris)
+class(iris) # "data.frame"
+
+irisTibble <- tibble::as_tibble(iris)
+class(irisTibble) # now a tibble
+
+
+# strings in R
+"Hello world"
+'Hello world'
+
+library("tidyverse")
+# install.packages("stringr")
+library("stringr")
+# install.packages("htmlwidgets")
+library("htmlwidgets")
+
+stringr::str_length("Hello world") # length is 11
+stringr::str_length("Hello world DSI") # 15
+
+userName <- c("Anjali", "Jessie") # character vector
+typeof(userName)
+stringr::str_c("Welcome to webpage of ", userName)
+stringr::str_c("Welcome to webpage of ", userName[1])
+?str_c
+
+stringr::str_sub("This is a string", 7, 12) # subsetting
+testDNA1 <- "TCGATTC"
+testDNA2 <- "GCGGGCA"
+stringr::str_c(testDNA1, stringr::str_sub(testDNA2, 4, 6))
+# TCGATTCGGC
+
+stringr::str_to_lower("UPPER")
+stringr::str_to_upper("upper")
+stringr::str_to_title("no capitalization")
+
+# matching patterns
+myString <- c("apple", "banana", "celementine", "dragonfruit")
+myString
+stringr::str_view(myString, "an")
+stringr::str_view_all(myString, "an")
+
+stringr::str_view_all(myString, ".a") # period indicate
+# any character except a new line
+stringr::str_view_all(myString, "^a") # ^ matches start
+stringr::str_view_all(myString, "a$") # $ define the end
+
+myString2 <- c("apple", "banana", "celementine", 
+               "dragonfruit", 1:4)
+myString2
+stringr::str_view_all(myString2, "\\d") #\d need an escape 
+# character
+
+myString3 <- c("apple pie", "banana", "celementine", 
+               "dragonfruit", 1:4)
+stringr::str_view_all(myString3, "\\s") # find white space
+
+# example
+install.packages("protr")
+library("protr")
+ls("package:protr") # list all the function
+?protr
+?readFASTA
+P00750 <- readFASTA(system.file("protseq/P00750.fasta",
+                                package = "protr"))
+P00750
+stringr::str_view_all(P00750, "CGA")
+stringr::str_view_all(P00750, "CG")
+stringr::str_count(P00750, "CG") # 4 
+
+# factors 
+library(forcats)
+?forcats
+letters
+month.abb
+monthsVector <- month.abb
+typeof(monthsVector)
+monthsVector
+
+monthsVectorFactor <- factor(monthsVector)
+monthsVectorFactor
+
+monthsVectorFactor2 <- factor(c(month.abb, month.abb))
+monthsVectorFactor2
+
+# recode factors - to alter coding for some categories
+forcats::fct_recode(monthsVectorFactor2, "December" = "Dec")
+
+forcats::fct_recode(monthsVectorFactor2, 
+                    "December" = "Dec",
+                    "March" = "Mar")
+
+# date
+install.packages("lubridate")
+library("lubridate")
+?lubridate
+lubridate::today()
+lubridate::now()
+
+date <- "20170102"
+lubridate::ymd(20170102) # "2017-01-02"
+
+lubridate::ydm(20170102) # "2017-02-01"
+
+lubridate::mdy("January 31st, 2017") # "2017-01-31"
+
+lubridate::dmy("31-Jan-2017") # "2017-01-31"
+
+# y is for year
+# m for month
+# d for date
+
+# h hours
+# m minutes
+# s for seconds
+
+dateTime <- lubridate::now()
+dateTime # "2022-11-12 10:41:16 EST"
+
+dateTimeFormatted <- lubridate::ymd_hms(dateTime, tz = "EST")
+lubridate::month(dateTimeFormatted) # month
+lubridate::mday(dateTimeFormatted) # day of the month
+lubridate::yday(dateTimeFormatted) # day of year
+
+# Missing data
+NA # Not applicable or not available; define missing data
+
+vectorNumeric <- c(1:5, NA)
+vectorNumeric
+length(vectorNumeric)
+
+is.na(vectorNumeric) # detect if NA values are present
+
+# Which of the values are not missing?
+vectorNumeric[is.na(vectorNumeric) == FALSE] # 1 2 3 4 5
+vectorNumeric[is.na(vectorNumeric) != TRUE] #  1 2 3 4 5
+
+objectName = 1 # assignment; not recommended
+objectName <- 1 # assignment;  recommended
+objectName == 1 # checking if equal
+objectName != 1 # checking those not equal to 
+
+# Which of the values are missing?
+vectorNumeric[is.na(vectorNumeric) == TRUE] # NA
+vectorNumeric[is.na(vectorNumeric) != FALSE] # NA
+
+vectorNumeric
+# To get the location of where the missing value is
+which(is.na(vectorNumeric) == TRUE)
+
+# To get the location of where the values are present; i.e.,
+# no missing values
+which(is.na(vectorNumeric) == FALSE)
+
+anyNA(vectorNumeric) # TRUE
+
+
+# Lesson: 04 on data manipulation
+tidyverse::tidyverse_packages()
+# install.packages("haven")
+library(haven)
+?haven
+ls("package:haven") # list all function
+
+# dataset we'll look at
+# Replication Data for: Assessing Public Value Failure in
+# Government Adoption of Artificial Intelligence
+# https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/LIGARA
+
+
+getwd() # /Users/as/Desktop/DSI-IntroR
+# produce error
+adsData <- haven::read_sav(file = "ads_raw.sav")
+
+# need to go to data subdirectory
+adsData <- haven::read_sav(file = "data/ads_raw.sav")
+adsData
+dim(adsData) # 1460   52
+pillar::glimpse(adsData)
+
+# dplyr: grammar of data manipulation
+# https://dplyr.tidyverse.org/  
+
+# filtering
+# permit to select rows based on specific criteria/trait
+pillar::glimpse(adsData)
+
+adsData$Duration__in_seconds_
+
+# check for missing values
+anyNA(adsData$Duration__in_seconds_) # FALSE
+
+?dplyr::filter
+filteredData <- dplyr::filter(adsData, 
+                              Duration__in_seconds_ < 100)
+filteredData # 41 X 52
+dim(filteredData) # 41 52
+
+# double check
+filteredData$Duration__in_seconds_
+range(filteredData$Duration__in_seconds_) # 50 99
+
+adsData$Gender
+table(adsData$Gender)
+# filter data to only contain males
+filterGender <- dplyr::filter(adsData, Gender == 1)
+filterGender # 758 × 52
+
+# arranging
+# permit to sort the order
+adsData$Duration__in_seconds_
+?dplyr::arrange
+
+arrangedData <- dplyr::arrange(adsData, Duration__in_seconds_)
+arrangedData
+dim(arrangedData) # 1460   52
+arrangedData$Duration__in_seconds_
+
+arrangedDataDescending <- dplyr::arrange(adsData, 
+                                         -Duration__in_seconds_)
+arrangedDataDescending$Duration__in_seconds_
+pillar::glimpse(arrangedDataDescending)
+
+
+# selecting
+# allows to select or remove columns
+?dplyr::select
+adsData$Duration__in_seconds_
+
+selectColumn <- dplyr::select(adsData, Duration__in_seconds_)
+selectColumn # 1,460 × 1
+dim(adsData) # 1460   52
+
+# remove a column
+removeColumn <- dplyr::select(adsData,
+                              -Duration__in_seconds_)
+removeColumn # 1460   51
+dim(removeColumn) # 1460   51
+dim(adsData) # 1460   52
+
+pillar::glimpse(adsData)
+adsData$Consent
+table(adsData$Consent)
+selectedData2 <- dplyr::select(adsData, -Consent)
+dim(selectedData2) # 1460   51
+
+# Answering question on chat
+questionOnChat <- dplyr::select(adsData, 
+                               Duration__in_seconds_,
+                               Consent)
+questionOnChat # 1,460 × 2
+pillar::glimpse(questionOnChat)
+# End of 12 Nov 2022
 
 # [End of File]
